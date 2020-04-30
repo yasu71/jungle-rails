3877952bd7f0c7eadea5ea2b3e7c8653dcd29a08
Yasu <yasu71@gmail.com>
2020-04-30 06:25:21 +0000

    Added admin dashboard features

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['ADMIN_USER'], password: ENV['ADMIN_PW']
 
   def show
+    @products_count = Product.count()
+    @categories_count = Category.count()
   end
 end
