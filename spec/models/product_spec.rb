6a4bb6c7504666ca4b6089ed2158cba7f3688189
yasu71 <yasuyo71@gmail.com>
2020-05-05 23:16:36 +0000

    Added Model spec: Product

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+
+  describe 'Validations' do
+
+    it 'is valid with valid attributes' do
+      @category = Category.create(name: "Cosmetics")
+      @product = @category.products.create(name: "Peach Perfect", price: 35, quantity: 50)
+      expect(@product).to be_valid
+    end
+
+    it "is not valid without a name" do
+      @category = Category.create(name: "Cosmetics")
+      @product = @category.products.create(price: 35, quantity: 50)
+      expect(@product).to_not be_valid
+    end
+
+    it "is not valid without a price" do
+      @category = Category.create(name: "Cosmetics")
+      @product = @category.products.create(name: "Peach Perfect",  quantity: 50)
+      expect(@product).to_not be_valid
+    end
+
+    it "is not valid without a quantity" do
+      @category = Category.create(name: "Cosmetics")
+      @product = @category.products.create(name: "Peach Perfect",  price: 35)
+      expect(@product).to_not be_valid
+    end
+
+    it "is not valid without a category" do
+      @product =Product.create(name: "Peach Perfect",  price: 35, quantity: 50)
+      expect(@product).to_not be_valid
+    end
+
+  end
+end
