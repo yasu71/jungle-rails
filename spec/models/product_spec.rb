1bc1eac959ee4984f6898eb45157c2d25fa0f19d
yasu71 <yasuyo71@gmail.com>
2020-05-06 00:49:01 +0000

    Added error messages

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -14,23 +14,27 @@ RSpec.describe Product, type: :model do
       @category = Category.create(name: "Cosmetics")
       @product = @category.products.create(price: 35, quantity: 50)
       expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to include("Name can't be blank")
     end
 
     it "is not valid without a price" do
       @category = Category.create(name: "Cosmetics")
       @product = @category.products.create(name: "Peach Perfect",  quantity: 50)
       expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to include("Price can't be blank")
     end
 
     it "is not valid without a quantity" do
       @category = Category.create(name: "Cosmetics")
       @product = @category.products.create(name: "Peach Perfect",  price: 35)
       expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to include("Quantity can't be blank")
     end
 
     it "is not valid without a category" do
       @product =Product.create(name: "Peach Perfect",  price: 35, quantity: 50)
       expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to include("Category can't be blank")
     end
 
   end
